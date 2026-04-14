131f22251f58c1b001d6347fe056402e3a0614c1
Vicky Shinde <vickyshinde30805@gmail.com>
2026-04-14 19:15:07 +0530

    practice of linked list

---
+++ Linked List/paractice.py
@@ -0,0 +1,61 @@
+class Node:
+     def __init__(self,data):
+          self.data=data
+          self.next=None
+
+def printLL(head):
+    curr=head
+    while curr!=None:
+         print(curr.data,end=" ")
+         curr=curr.next
+    print()
+
+# Create linked list
+a=Node(5)
+b=Node(7)
+c=Node(2)
+a.next=b
+b.next=c
+head=a
+
+# Insert at beginning
+newNode=Node(3)
+newNode.next=head
+head =newNode
+
+#insert at the end
+endNode=Node(8)
+curr=head
+while curr.next!=None:
+     curr=curr.next
+curr.next=endNode
+
+#insert at the middle
+middleNode=Node(9)
+k=4
+curr=head
+for i in range(k-2):
+     curr=curr.next
+middleNode.next=curr.next
+curr.next=middleNode
+
+#remove at beggining
+head=head.next
+
+#remove at end
+curr=head
+while curr.next.next!=None:
+     curr=curr.next
+curr.next=None
+
+#remove at middle
+k=3
+curr=head 
+for i in range(k-2):
+     curr=curr.next
+curr.next=curr.next.next
+
+     
+
+
+printLL(head)
